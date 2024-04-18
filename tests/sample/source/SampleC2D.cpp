e6f60ed054bb7fe5cc98317912a1beb681396d01
kaibfang <kaibfang@qti.qualcomm.com>
2024-04-18 17:34:20 +0800

    add performance profiler in SampleC2D

--- tests/sample/source/SampleC2D.cpp
+++ tests/sample/source/SampleC2D.cpp
@@ -196,9 +196,11 @@ void SampleC2D::ThreadMain()
                     inputs.push_back( frame.buffer->sharedBuffer );
                 }
 
-                ret = m_c2d.Execute( inputs.data(), inputs.size(), &buffer->sharedBuffer );
+                PROFILER_BEGIN();
+		ret = m_c2d.Execute( inputs.data(), inputs.size(), &buffer->sharedBuffer );
                 if ( RIDE_HAL_ERROR_NONE == ret )
                 {
+		    PROFILER_END();
                     CamFrames_t outFrames;
                     CamFrame_t frame;
                     frame.buffer = buffer;
@@ -228,6 +230,8 @@ RideHalError_e SampleC2D::Stop()
     }
 
     ret = m_c2d.Stop();
+
+    PROFILER_SHOW();
 
     return ret;
 }
