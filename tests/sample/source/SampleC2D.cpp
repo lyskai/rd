fec03fcd773a5cd9c923848f63db2621ca1b69a6
Kaibo Fang <kaibfang@qti.qualcomm.com>
2024-07-30 09:24:57 +0000

    Add gtest accuracy cases for C2D

--- tests/sample/source/SampleC2D.cpp
+++ tests/sample/source/SampleC2D.cpp
@@ -215,6 +215,8 @@ void SampleC2D::ThreadMain()
 
                 PROFILER_BEGIN();
                 TRACE_BEGIN( frames.FrameId( 0 ) );
+                void *pOutputData = buffer->sharedBuffer.data();
+                memset( pOutputData, 0, buffer->sharedBuffer.size );
                 ret = m_c2d.Execute( inputs.data(), inputs.size(), &buffer->sharedBuffer );
                 if ( RIDEHAL_ERROR_NONE == ret )
                 {
@@ -272,3 +274,4 @@ REGISTER_SAMPLE( C2D, SampleC2D );
 
 }   // namespace sample
 }   // namespace ridehal
+
--- tests/unit_test/components/C2D/gtest_C2D.cpp
+++ tests/unit_test/components/C2D/gtest_C2D.cpp
@@ -1,13 +1,77 @@
 // Copyright 2024 Qualcomm Technologies, Inc. All rights reserved.
 // Confidential & Proprietary - Qualcomm Technologies, Inc. ("QTI")
 
-#include "gtest/gtest.h"
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
+#include "md5_utils.hpp"
 #include "ridehal/component/C2D.hpp"
+#include "gtest/gtest.h"
 
 using namespace ridehal::common;
 using namespace ridehal::component;
+using namespace ridehal::test::utils;
+
+void readTXT( char *file_path, char *data_list[] )
+{
+    char line[100];
+    FILE *file = fopen( file_path, "r" );
+
+    if ( file != NULL )
+    {
+        int i = 0;
+        while ( fgets( line, sizeof( line ), file ) != NULL )
+        {
+            data_list[i] = (char *) malloc( strlen( line ) + 1 );
+            line[strcspn( line, "\n" )] = '\0';
+            strcpy( data_list[i++], line );
+        }
+        fclose( file );
+    }
+    else
+    {
+        printf( "Could not open %s\n", file_path );
+        return;
+    }
+
+    return;
+}
+
+void loadRawData( char *file_path, void *pData, size_t data_size )
+{
+    FILE *file = fopen( file_path, "rb" );
+
+    if ( file != NULL )
+    {
+        fseek( file, 0, SEEK_END );
+        size_t fsize = ftell( file );
+        fseek( file, 0, SEEK_SET );
+        if ( fsize == data_size )
+        {
+            if ( pData != nullptr )
+            {
+                size_t readSize = fread( pData, 1, fsize, file );
+                if ( fsize != readSize )
+                {
+                    printf( "read size for %s does not match, read size: %lu, file size: %lu\n",
+                            file_path, readSize, fsize );
+                }
+            }
+            else
+            {
+                printf( "pData is null pointer\n" );
+            }
+        }
+        else
+        {
+            printf( "data size for %s does not match, input size: %lu, file size: %lu\n", file_path,
+                    data_size, fsize );
+        }
+    }
+    fclose( file );
+}
+
 
 void C2DTestNormal( C2D_Config_t *c2dConfig, RideHal_ImageFormat_e outputFormat,
                     uint32_t outputWidth, uint32_t outputHeight )
@@ -57,6 +121,77 @@ void C2DTestNormal( C2D_Config_t *c2dConfig, RideHal_ImageFormat_e outputFormat,
 
     ret = C2DObj.DeregisterOutputBuffers( &output, 1 );
     ASSERT_EQ( RIDEHAL_ERROR_NONE, ret );
+
+    ret = C2DObj.Stop();
+    ASSERT_EQ( RIDEHAL_ERROR_NONE, ret );
+
+    ret = C2DObj.Deinit();
+    ASSERT_EQ( RIDEHAL_ERROR_NONE, ret );
+}
+
+void C2DTestAccuracy( C2D_Config_t *c2dConfig, RideHal_ImageFormat_e outputFormat,
+                      uint32_t outputWidth, uint32_t outputHeight, char *data_list_file,
+                      char *golden_list_file )
+{
+    RideHalError_e ret = RIDEHAL_ERROR_NONE;
+
+    C2D C2DObj;
+    char pName[5] = "C2D";
+    uint32_t numInputs = c2dConfig->numOfInputs;
+    RideHal_SharedBuffer_t inputs[numInputs];
+    RideHal_SharedBuffer_t output;
+
+    char *data_list[numInputs];
+    char *golden_list[numInputs];
+    void *golden_data[numInputs];
+    void *pOutputData = nullptr;
+
+    readTXT( data_list_file, data_list );
+    readTXT( golden_list_file, golden_list );
+
+    ret = C2DObj.Init( pName, c2dConfig );
+    ASSERT_EQ( RIDEHAL_ERROR_NONE, ret );
+
+    for ( size_t i = 0; i < numInputs; i++ )
+    {
+        ret = inputs[i].Allocate( c2dConfig->inputConfigs[i].inputResolution.width,
+                                  c2dConfig->inputConfigs[i].inputResolution.height,
+                                  c2dConfig->inputConfigs[i].inputFormat );
+        ASSERT_EQ( RIDEHAL_ERROR_NONE, ret );
+
+        loadRawData( data_list[i], inputs[i].data(), inputs[i].size );
+    }
+
+    ret = output.Allocate( numInputs, outputWidth, outputHeight, outputFormat );
+    ASSERT_EQ( RIDEHAL_ERROR_NONE, ret );
+
+    memset( output.data(), 0, output.size );
+
+    uint32_t outputSize = output.size / numInputs;
+    for ( size_t i = 0; i < numInputs; i++ )
+    {
+        golden_data[i] = (void *) malloc( outputSize );
+        loadRawData( golden_list[i], golden_data[i], outputSize );
+    }
+
+    ret = C2DObj.Start();
+    ASSERT_EQ( RIDEHAL_ERROR_NONE, ret );
+
+    ret = C2DObj.Execute( inputs, numInputs, &output );
+    ASSERT_EQ( RIDEHAL_ERROR_NONE, ret );
+
+    for ( size_t i = 0; i < numInputs; i++ )
+    {
+        pOutputData = (void *) ( (uint8_t *) output.data() + i * outputSize );
+        std::string md5OutputGolden = MD5Sum( pOutputData, outputSize );
+        std::string md5OutputData = MD5Sum( golden_data[i], outputSize );
+        EXPECT_EQ( md5OutputGolden, md5OutputData );
+
+        printf( "md5sum for output batch %lu: %s\n", i, md5OutputData.c_str() );
+        printf( "md5sum for golden data %lu: %s\n", i, md5OutputGolden.c_str() );
+
+        free( golden_data[i] );
+    }
 
     ret = C2DObj.Stop();
     ASSERT_EQ( RIDEHAL_ERROR_NONE, ret );
@@ -668,7 +803,8 @@ TEST( C2D, FAILURE_C2D_GetSourceSurf )
     ASSERT_EQ( RIDEHAL_ERROR_NONE, ret );
 
     RideHal_SharedBuffer_t output;
-    ret = output.Allocate( C2DOutputWidth, C2DOutputHeight, C2DOutputFormat );
+    ret = output.Allocate( C2DConfig.numOfInputs, C2DOutputWidth, C2DOutputHeight,
+                           C2DOutputFormat );
     ASSERT_EQ( RIDEHAL_ERROR_NONE, ret );
 
     ret = C2DObj.Init( pName, pC2DConfig );
@@ -986,6 +1122,60 @@ TEST( C2D, FAILURE_C2D_DeRegOutputBuffer )
 
     ret = C2DObj.Deinit();
     ASSERT_EQ( RIDEHAL_ERROR_NONE, ret );
+}
+
+TEST( C2D, ACCURACY_C2D_ConvertNV12toUYVY_4Batch_ROI )
+{
+    C2D_Config_t C2DConfig;
+    C2D_Config_t *pC2DConfig = &C2DConfig;
+    char input_file_list[] = "data/test/c2d/input_data/NV12/input_data_list.txt";
+    char golden_file_list[] = "data/test/c2d/golden_data/UYVY/golden_data_list.txt";
+
+    pC2DConfig->numOfInputs = 4;
+    for ( size_t i = 0; i < C2DConfig.numOfInputs; i++ )
+    {
+        pC2DConfig->inputConfigs[i].inputFormat = RIDEHAL_IMAGE_FORMAT_NV12;
+        pC2DConfig->inputConfigs[i].inputResolution.width = 1920;
+        pC2DConfig->inputConfigs[i].inputResolution.height = 1024;
+        pC2DConfig->inputConfigs[i].ROI.topX = 100;
+        pC2DConfig->inputConfigs[i].ROI.topY = 100;
+        pC2DConfig->inputConfigs[i].ROI.width = 600;
+        pC2DConfig->inputConfigs[i].ROI.height = 600;
+    }
+
+    RideHal_ImageFormat_e outputFormat = RIDEHAL_IMAGE_FORMAT_UYVY;
+    uint32_t outputWidth = 1024;
+    uint32_t outputHeight = 768;
+
+    C2DTestAccuracy( pC2DConfig, outputFormat, outputWidth, outputHeight, input_file_list,
+                     golden_file_list );
+}
+
+TEST( C2D, ACCURACY_C2D_ConvertNV12toRGB_4Batch_ROI )
+{
+    C2D_Config_t C2DConfig;
+    C2D_Config_t *pC2DConfig = &C2DConfig;
+    char input_file_list[] = "data/test/c2d/input_data/NV12/input_data_list.txt";
+    char golden_file_list[] = "data/test/c2d/golden_data/RGB/golden_data_list.txt";
+
+    pC2DConfig->numOfInputs = 4;
+    for ( size_t i = 0; i < C2DConfig.numOfInputs; i++ )
+    {
+        pC2DConfig->inputConfigs[i].inputFormat = RIDEHAL_IMAGE_FORMAT_NV12;
+        pC2DConfig->inputConfigs[i].inputResolution.width = 1920;
+        pC2DConfig->inputConfigs[i].inputResolution.height = 1024;
+        pC2DConfig->inputConfigs[i].ROI.topX = 100;
+        pC2DConfig->inputConfigs[i].ROI.topY = 100;
+        pC2DConfig->inputConfigs[i].ROI.width = 600;
+        pC2DConfig->inputConfigs[i].ROI.height = 600;
+    }
+
+    RideHal_ImageFormat_e outputFormat = RIDEHAL_IMAGE_FORMAT_RGB888;
+    uint32_t outputWidth = 1024;
+    uint32_t outputHeight = 768;
+
+    C2DTestAccuracy( pC2DConfig, outputFormat, outputWidth, outputHeight, input_file_list,
+                     golden_file_list );
 }
 
 
