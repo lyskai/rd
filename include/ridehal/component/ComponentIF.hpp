addc5bd8f48ab023368a8b16ac6040617046346f
Parai Wang <fpwang@qti.qualcomm.com>
2024-07-31 16:16:38 +0800

    Create RideHal 1.2.0 release
    Change-Id: I54b4fd74ea02ff0c087b34fd0515419147f5d39b
    Signed-off-by: Parai Wang <fpwang@qti.qualcomm.com>

--- include/ridehal/component/ComponentIF.hpp
+++ include/ridehal/component/ComponentIF.hpp
@@ -14,7 +14,7 @@ using namespace ridehal::common;
 
 /** @brief The RideHal Version */
 #define RIDEHAL_VERSION_MAJOR 1
-#define RIDEHAL_VERSION_MINOR 1
+#define RIDEHAL_VERSION_MINOR 2
 #define RIDEHAL_VERSION_PATCH 0
 
 namespace ridehal
