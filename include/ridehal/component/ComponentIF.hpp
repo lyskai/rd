a970c4668a00d5f89ec63f8f9a47903b72a84d03
Parai Wang <fpwang@qti.qualcomm.com>
2024-10-31 10:21:59 +0800

    Promote version to 1.5.0
    Change-Id: Ie01d7b20099659f5145b9939cd9a253fd32b9ca5
    Signed-off-by: Parai Wang <fpwang@qti.qualcomm.com>

--- include/ridehal/component/ComponentIF.hpp
+++ include/ridehal/component/ComponentIF.hpp
@@ -16,8 +16,8 @@ using namespace ridehal::common;
 
 /** @brief The RideHal Version */
 #define RIDEHAL_VERSION_MAJOR 1
-#define RIDEHAL_VERSION_MINOR 4
-#define RIDEHAL_VERSION_PATCH 1
+#define RIDEHAL_VERSION_MINOR 5
+#define RIDEHAL_VERSION_PATCH 0
 
 namespace ridehal
 {
