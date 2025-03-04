da3c60ab2f29131341e44b25d35742f42db7ba21
Kaibo Fang <kaibfang@qti.qualcomm.com>
2025-03-04 10:12:07 +0000

    Promote RideHal version to v1.8.0

--- include/ridehal/component/ComponentIF.hpp
+++ include/ridehal/component/ComponentIF.hpp
@@ -16,7 +16,7 @@ using namespace ridehal::common;
 
 /** @brief The RideHal Version */
 #define RIDEHAL_VERSION_MAJOR 1
-#define RIDEHAL_VERSION_MINOR 7
+#define RIDEHAL_VERSION_MINOR 8
 #define RIDEHAL_VERSION_PATCH 0
 
 namespace ridehal
